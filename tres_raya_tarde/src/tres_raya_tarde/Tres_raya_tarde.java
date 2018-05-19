813d222186d4f825331c638966e4e3cb5df39f82
maxfv11 <maxinformatica123@gmail.com>
2018-05-19 16:59:14 +0200

    commit inici
    commit inici del projecte
    afegit projecte netbeans

---
+++ tres_raya_tarde/src/tres_raya_tarde/Tres_raya_tarde.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tres_raya_tarde;
+
+/**
+ *
+ * @author Max
+ */
+public class Tres_raya_tarde {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
